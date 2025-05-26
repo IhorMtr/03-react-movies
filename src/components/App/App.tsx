ea525c97c2ef6427aadbc3e129881c96999799d6
IhorMtr <igormotornyyy@gmail.com>
2025-05-26 23:14:52 +0300

    finish

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,60 @@
+import SearchBar from '../SearchBar/SearchBar';
 import css from './App.module.css';
+import fetchMovies from '../../services/movieService';
+import toast, { Toaster } from 'react-hot-toast';
+import type { Movie } from '../../types/movies.ts';
+import { useState } from 'react';
+import MovieGrid from '../MovieGrid/MovieGrid.tsx';
+import Loader from '../Loader/Loader.tsx';
+import ErrorMessage from '../ErrorMessage/ErrorMessage.tsx';
+import MovieModal from '../MovieModal/MovieModal.tsx';
+
+const notify = () => toast.error('No movies found for your request.');
+
 export default function App() {
-  return <div className={css.app}></div>;
+  const [films, setFilms] = useState<Movie[]>([]);
+  const [isLoader, setIsLoader] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [selectedFilm, setSelectedFilm] = useState<Movie | null>(null);
+
+  const handleSearch = async (film: string): Promise<void> => {
+    try {
+      setIsError(false);
+      setFilms([]);
+      setIsLoader(true);
+      const data = await fetchMovies(film);
+      if (data.length === 0) {
+        notify();
+      } else {
+        setFilms(data);
+      }
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoader(false);
+    }
+  };
+
+  const handleChoice = (film: Movie): void => {
+    setSelectedFilm(film);
+  };
+
+  const handleClosing = (): void => {
+    setSelectedFilm(null);
+  };
+
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={handleSearch} />
+      {isLoader && <Loader />}
+      {isError && <ErrorMessage />}
+      <Toaster />
+      {films.length !== 0 && (
+        <MovieGrid onSelect={handleChoice} movies={films} />
+      )}
+      {selectedFilm && (
+        <MovieModal movie={selectedFilm} onClose={handleClosing} />
+      )}
+    </div>
+  );
 }
---
+++ src/services/movieService.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { type Movie } from '../types/movies.ts';
+interface MoviesResponse {
+  results: Movie[];
+}
+
+export default async function fetchMovies(
+  inputQuery: string
+): Promise<Movie[]> {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const options = {
+    method: 'get',
+    url: 'https://api.themoviedb.org/3/search/movie',
+    params: { query: inputQuery },
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+    },
+  };
+
+  const response = await axios<MoviesResponse>(options);
+  return response.data.results;
+}
