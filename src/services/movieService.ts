aa0046d60b28025bb8c4750f59e28c965c2c8c68
IhorMtr <igormotornyyy@gmail.com>
2025-05-27 15:14:36 +0300

    hotfix

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { type Movie } from '../types/movie.ts';
+
 interface MoviesResponse {
   results: Movie[];
 }
@@ -8,16 +9,17 @@ export default async function fetchMovies(
   inputQuery: string
 ): Promise<Movie[]> {
   const apiKey = import.meta.env.VITE_API_KEY;
-  const options = {
-    method: 'get',
-    url: 'https://api.themoviedb.org/3/search/movie',
-    params: { query: inputQuery },
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
 
-  const response = await axios<MoviesResponse>(options);
+  const response = await axios.get<MoviesResponse>(
+    'https://api.themoviedb.org/3/search/movie',
+    {
+      params: { query: inputQuery },
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    }
+  );
+
   return response.data.results;
 }
